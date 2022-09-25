cb639925816b02ddef8a88e3abe8431306beb97e
Vishwa Vijeta <vishwavijet1@gmail.com>
2022-09-25 13:43:32 +0530

    setup static files access for layouts and page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req,res){
-    return res.end('<h1> Express is up for Fbclone</h1>')
+    
+    return res.render('home', {
+        title: "Home"
+    });
 }
 
 // module.exports.actionName = fuction(req,res){}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,16 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile</h1>');
+    
+    return res.render('user_profile', {
+        title: "User Profile"
+    });
 }   
+
+
+
+
+
+
+
+// module.exports.profile = function(req,res){
+//     res.end('<h1>User Profile</h1>');
+// }   
