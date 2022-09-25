47bfaf9895aba24c66d25d8e3498b5644234dc17
Vishwa Vijeta <vishwavijet1@gmail.com>
2022-09-25 15:31:06 +0530

    Create a sign up and sign in page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,13 +3,31 @@ module.exports.profile = function(req,res){
     return res.render('user_profile', {
         title: "User Profile"
     });
-}   
+};   
 
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "fbclone | Sign Up"
+    });
+};
 
+// render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: "fbclone | Sign In"
+    });
+};
 
+//get the sign up data
+module.exports.create = function(req, res){
+    //TODO later
+}
 
-
-
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    //TODO later
+}
 
 // module.exports.profile = function(req,res){
 //     res.end('<h1>User Profile</h1>');
