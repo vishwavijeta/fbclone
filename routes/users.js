dd36b23a2b4d80953d789dda710d2c3d09eaaeb3
Vishwa Vijeta <vishwavijet1@gmail.com>
2022-09-25 20:46:43 +0530

    Created a Sign up for a new user

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,6 @@ router.get('/profile', userController.profile);
 router.get('/sign-up', userController.signUp);
 router.get('/sign-in', userController.signIn);
 
+router.post('/create', userController.create);
+
 module.exports = router;
