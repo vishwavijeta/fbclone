e74e1bb28fb50939a76e46bbf96823ac4f5e84f6
Vishwa Vijeta <vishwavijet1@gmail.com>
2022-09-27 14:37:46 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    
-    return res.render('home', {
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "fb | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: " Codeial | Home",
+            posts : posts
+        });
     });
+    
 }
 
 // module.exports.actionName = fuction(req,res){}
